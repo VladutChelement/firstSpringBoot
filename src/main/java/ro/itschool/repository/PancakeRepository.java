49b68190a35577eeaf11e9d5157344bc8713f792
VladutChelement <vladut.chelement@gmail.com>
2022-06-22 13:44:24 +0300

    Initial commit

---
+++ src/main/java/ro/itschool/repository/PancakeRepository.java
@@ -0,0 +1,12 @@
+package ro.itschool.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ro.itschool.entity.Pancake;
+
+public interface PancakeRepository extends JpaRepository<Pancake, Integer> {
+
+
+
+
+
+}
